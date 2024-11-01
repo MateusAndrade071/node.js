d4763224437ff2f628d87a0197df209e4e782351
Mateus Adrade <128805906+MateusAndrade071@users.noreply.github.com>
2024-11-01 17:14:59 -0300

    Add files via upload

---
+++ aula1-modulos/calc.js
@@ -0,0 +1,4 @@
+// importando a função soma do arquivo soma.js
+var somaFunc = require("./soma.js");
+// utilizando a função soma
+console.log(somaFunc(2,3));
---
+++ aula1-modulos/soma.js
@@ -0,0 +1,6 @@
+// criando uma função de soma 
+var soma = function(a,b){
+    return a + b;
+};
+// utilizado para exportar a funçao para para que ela possa ser utilizada em outros arquivos.
+module.exports = soma;
